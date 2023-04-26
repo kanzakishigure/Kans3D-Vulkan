1174d4f3acfead631d60dc25ce09d3e3040d198f
kanzakishigure <1214005662@qq.com>
2023-04-26 11:21:35 +0800

    move from Base.h to Ref.h file

--- Kans3D/src/Kans3D/Core/Base/Base.h
+++ Kans3D/src/Kans3D/Core/Base/Base.h
@@ -3,41 +3,14 @@
 #include <atomic>
 #include "Kans3D/Core/PlatformDetection.h"
 
-
-
-#ifdef HZ_DEBUG
-	#define HZ_ENABLE_ASSERTS
-#endif // HZ_DEBUG
-
-#ifdef HZ_ENABLE_ASSERTS
-	#define HZ_ASSERT(x,...) {if(!(x)){HZ_ERROR("Assertion Failed:{0}",__VA_ARGS__);__debugbreak();}}
-	#define HZ_CORE_ASSERT(x,...) {if(!(x)){HZ_CORE_ERROR("Assertion Failed:{0}",__VA_ARGS__);__debugbreak();}}
-#else
-	#define HZ_ASSERT(x,...)
-	#define HZ_CORE_ASSERT(x,...)
-#endif // HZ_ENABLE_ASSERTS
-
+#include "Ref.h"
 
 #define BIT(x) (1<<x)
-#define HZ_BIND_EVENT_FN(fn) std::bind(&fn,this,std::placeholders::_1)
+#define BIND_EVENT_FN(fn) std::bind(&fn,this,std::placeholders::_1)
 
+
+#include "Kans3D/Core/Base/Assert.h"
 namespace Kans
 {
-	template <typename T> 
-	using Scope = std::unique_ptr<T>;
-	template <typename T,typename ...Args>
-	constexpr Scope<T> CreateScope(Args&& ... args )
-	{
-		return std::make_unique<T>(std::forward<Args>(args)...);
-	}
-	template <typename T>
-	using Ref = std::shared_ptr<T>;
-	template <typename T, typename ...Args>
-	constexpr Ref<T> CreateRef(Args&& ... args)
-	{
-		return std::make_shared<T>(std::forward<Args>(args)...);
-	}
-
-
 	using byte = uint8_t;
 }
--- Kans3D/src/Kans3D/Core/Base/Ref.h
+++ Kans3D/src/Kans3D/Core/Base/Ref.h
@@ -2,6 +2,7 @@
 
 #include<atomic>
 #include "Memory.h"
+#include <memory>
 namespace Kans
 {
 	class RefCounter
@@ -20,4 +21,26 @@ namespace Kans
 		mutable std::atomic<uint32_t> m_RefCount = 0;
 
 	};
+
+	template <typename T>
+	using Scope = std::unique_ptr<T>;
+	template <typename T, typename ...Args>
+	constexpr Scope<T> CreateScope(Args&& ... args)
+	{
+		return std::make_unique<T>(std::forward<Args>(args)...);
+	}
+
+	template <typename T>
+	using Ref = std::shared_ptr<T>;
+	template <typename T, typename ...Args>
+	constexpr Ref<T> CreateRef(Args&& ... args)
+	{
+		return std::make_shared<T>(std::forward<Args>(args)...);
+	}
+
+	
+
+	template <typename T>
+	using WeakRef = std::weak_ptr<T>;
+
 }
