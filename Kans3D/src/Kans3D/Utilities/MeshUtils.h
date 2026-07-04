0f77528f18e3adf7e9da3526a98cb6f1630fa50e
Kanzaki <1214005662@qq.com>
2026-07-04 14:30:35 +0800

    perf(util): add hash-based smooth normal and fix mesh offset utilities
    Implement SmoothNormalHash (O(n) via unordered_map) to replace O(n²)

--- Kans3D/src/Kans3D/Utilities/MeshUtils.h
+++ Kans3D/src/Kans3D/Utilities/MeshUtils.h
@@ -12,7 +12,7 @@ namespace Kans::Utils
 	{
 	public:
 		static void SmoothNormal(std::vector<Vertex>& verteices, std::vector<glm::vec3>& smoothedNormal);
-		static void FastSmoothNormal(std::vector<Vertex>& verteices);
+		static void SmoothNormalHash(std::vector<Vertex>& verteices, std::vector<glm::vec3>& smoothedNormal);
 		static void SetMeshOffset(std::vector<Vertex>& verteices, const std::vector<SubMesh>& submushes, float offset);
 		static void BackMeshNormal(std::vector<Vertex>& verteices, const std::vector<glm::vec3>& backedNormal);
 	};
